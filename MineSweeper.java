824bd0d09e75a6481e920147988faf9a93835f2d
NicholasVella08 <103609198+NicholasVella08@users.noreply.github.com>
2022-12-25 17:15:49 +0100

    creating playMove() and fixNeighbours()

--- MineSweeper.java
+++ MineSweeper.java
@@ -127,4 +127,181 @@ public class MineSweeper {
         }
     }
 
+    public boolean playMove()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter Row Number: ");
+        int i= sc.nextInt();
+        System.out.print("Enter Column Number: ");
+        int j= sc.nextInt();
+
+        if(i<0 || i>9 || j<0 || j>9 || fieldVisible[i][j]!=0)
+        {
+            System.out.print("\nIncorrect Input!!");
+            return true;
+        }
+
+        if(fieldHidden[i][j]==100)
+        {
+            displayHidden();
+            System.out.print("Oops! You stepped on a mine!\n============GAME OVER============");
+            return false;
+        }
+        else if(fieldHidden[i][j]==0)
+        {
+            fixVisible(i, j);
+        }
+        else
+        {
+            fixNeighbours(i, j);
+        }
+
+        return true;
+    }
+
+
+    public void fixVisible(int i, int j)
+    {
+        fieldVisible[i][j] = 50;
+        if(i!=0)
+        {
+            fieldVisible[i-1][j] = fieldHidden[i-1][j];
+            if(fieldVisible[i-1][j]==0) fieldVisible[i-1][j] = 50;
+            if(j!=0)
+            {
+                fieldVisible[i-1][j-1] = fieldHidden[i-1][j-1];
+                if(fieldVisible[i-1][j-1]==0) fieldVisible[i-1][j-1]=50;
+
+            }
+        }
+        if(i!=9)
+        {
+            fieldVisible[i+1][j]=fieldHidden[i+1][j];
+            if(fieldVisible[i+1][j]==0) fieldVisible[i+1][j]=50;
+            if(j!=9)
+            {
+                fieldVisible[i+1][j+1]= fieldHidden[i+1][j+1];
+                if(fieldVisible[i+1][j+1]==0) fieldVisible[i+1][j+1] = 50;
+            }
+        }
+        if(j!=0)
+        {
+            fieldVisible[i][j-1]=fieldHidden[i][j-1];
+            if(fieldVisible[i][j-1]==0) fieldVisible[i][j-1] = 50;
+            if(i!=9)
+            {
+                fieldVisible[i+1][j-1]=fieldHidden[i+1][j-1];
+                if(fieldVisible[i+1][j-1]==0) fieldVisible[i+1][j-1] = 50;
+            }
+        }
+        if(j!=9)
+        {
+            fieldVisible[i][j+1]=fieldHidden[i][j+1];
+            if(fieldVisible[i][j+1]==0) fieldVisible[i][j+1] = 50;
+            if(i!=0)
+            {
+                fieldVisible[i-1][j+1]=fieldHidden[i-1][j+1];
+                if(fieldVisible[i-1][j+1]==0) fieldVisible[i-1][j+1] = 50;
+            }
+        }
+    }
+
+    public void fixNeighbours(int i, int j)
+    {
+        Random random = new Random();
+        int x = random.nextInt()%4;
+
+        fieldVisible[i][j] = fieldHidden[i][j];
+
+        if(x==0)
+        {
+            
+        }
+        else if(x==1)
+        {
+            if(i!=0)
+            {
+                if(fieldHidden[i-1][j]!=100)
+                {
+                    fieldVisible[i-1][j] = fieldHidden[i-1][j];
+                    if(fieldVisible[i-1][j]==0)  fieldVisible[i-1][j] = 50;
+                }
+            }
+            if(j!=9)
+            {
+                if(fieldHidden[i][j+1]!=100)
+                {
+                    fieldVisible[i][j+1] = fieldHidden[i][j+1];
+                    if(fieldVisible[i][j+1]==0)  fieldVisible[i][j+1] = 50;
+                }
+
+            }
+            if(i!=0 && j!=9)
+            {
+                if(fieldHidden[i-1][j+1]!=100)
+                {
+                    fieldVisible[i-1][j+1] = fieldHidden[i-1][j+1];
+                    if(fieldVisible[i-1][j+1]==0)  fieldVisible[i-1][j+1] = 50;
+                }
+            }
+        }
+        else if(x==2)
+        {
+            if(i!=9)
+            {
+                if(fieldHidden[i+1][j]!=100)
+                {
+                    fieldVisible[i+1][j] = fieldHidden[i+1][j];
+                    if(fieldVisible[i+1][j]==0)  fieldVisible[i+1][j] = 50;
+                }
+            }
+            if(j!=9)
+            {
+                if(fieldHidden[i][j+1]!=100)
+                {
+                    fieldVisible[i][j+1] = fieldHidden[i][j+1];
+                    if(fieldVisible[i][j+1]==0)  fieldVisible[i][j+1] = 50;
+                }
+
+            }
+            if(i!=9 && j!=9)
+            {
+                if(fieldHidden[i+1][j+1]!=100)
+                {
+                    fieldVisible[i+1][j+1] = fieldHidden[i+1][j+1];
+                    if(fieldVisible[i+1][j+1]==0)  fieldVisible[i+1][j+1] = 50;
+                }
+            }
+        }
+        else
+        {
+            if(i!=9)
+            {
+                if(fieldHidden[i+1][j]!=100)
+                {
+                    fieldVisible[i+1][j] = fieldHidden[i+1][j];
+                    if(fieldVisible[i+1][j]==0)  fieldVisible[i+1][j] = 50;
+                }
+            }
+            if(j!=0)
+            {
+                if(fieldHidden[i][j-1]!=100)
+                {
+                    fieldVisible[i][j-1] = fieldHidden[i][j-1];
+                    if(fieldVisible[i][j-1]==0)  fieldVisible[i][j-1] = 50;
+                }
+
+            }
+            if(i!=9 && j!=0)
+            {
+                if(fieldHidden[i+1][j-1]!=100)
+                {
+                    fieldVisible[i+1][j-1] = fieldHidden[i+1][j-1];
+                    if(fieldVisible[i+1][j-1]==0)  fieldVisible[i+1][j-1] = 50;
+                }
+            }
+        }
+    }
+
+
 }
